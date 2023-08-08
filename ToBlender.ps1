c1864c4b73dfefe6aba38fdfd8aece4945964646
LtSten <ltsten-public@outlook.com>
2023-08-08 18:09:09 +0100

    Update development script to work in appdata rather than program files.

--- ToBlender.ps1
+++ ToBlender.ps1
@@ -1,7 +1,7 @@
-$BlenderVer = "3.4"
-$BlenderDir = "D:\Program Files\Blender Foundation\Blender $BlenderVer\$BlenderVer"
+$BlenderVer = "3.6"
+$BlenderDir = "$env:APPDATA\Blender Foundation\Blender\$BlenderVer"
 $OutputDir = "$BlenderDir\scripts\addons\io_mesh_tpm"
 Write-Output "Copying files to '$OutputDir'"
 
 # Won't create the directory if it doesn't exist (want to fail loudly rather than silently)
-Copy-Item -Path "*.py" -Destination "$OutputDir"
+Copy-Item -Path ".\io_mesh_tpm\*.py" -Destination "$OutputDir"
